9a6597f4bd3da694fd977836f835657e7c1d8bf4
SethBowman <brandonsethbowman@gmail.com>
2023-10-06 14:33:20 -0500

    Add project files.

---
+++ ClassesDemoCSharp-41/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDemoCSharp_41
+{
+    public class Person
+    {
+        //Field - a variable inside of a class
+        public int money;
+        public string clothes;
+
+        //Properties
+        public string Name { get; set; } = "Bob";
+        public int Age { get; set; } = 20;
+        public string HairColor { get; set; } = "blonde";
+
+        //Constructor
+
+        //Default constructor
+        public Person()
+        {
+
+        }
+
+        //Custom constructor
+        public Person(string name, int age, string hairColor)
+        {
+            Name = name;
+            Age = age;
+            HairColor = hairColor;
+        }      
+       
+
+
+        //Methods in a class (non static)
+        public void Greet()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+        }
+
+    }
+}
---
+++ ClassesDemoCSharp-41/Program.cs
@@ -0,0 +1,40 @@
+﻿using ClassesDemoCSharp_41;
+
+//Instance of the classroom class so we can use the list it holds as property
+var classroom = new Classroom();
+
+//Instace of a class (use the constructor to actually build an object or instance)
+
+Person seth = new Person();
+
+//Dot notation to fill the values
+seth.Name = "Seth";
+seth.Age = 29;
+seth.HairColor = "black";
+
+//Adding this person to our list
+classroom.OurClass.Add(seth);
+
+
+//Object initializer syntax
+
+var jeremy = new Person() 
+{ 
+    Name = "Jeremy",
+    Age = 30,
+    HairColor = "blonde"
+};
+
+
+//Adding this person to our list
+classroom.OurClass.Add(jeremy);
+
+//Using a custom constructor
+
+var cruz = new Person("Cruz", 32, "black");
+
+//Adding this person to our list
+classroom.OurClass.Add(cruz);
+
+classroom.GetDetails();
+
